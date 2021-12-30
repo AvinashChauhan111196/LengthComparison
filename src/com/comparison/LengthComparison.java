59d91945512c2986b16e19d45cadfdf15c14cbb6
Avinash <Avichauhan111196@gmail.com>
2021-12-30 23:10:22 +0530

    Added : Equality of Line added

--- src/com/comparison/LengthComparison.java
+++ src/com/comparison/LengthComparison.java
@@ -1,18 +1,28 @@
 package com.comparison;
 
-public class LengthComparison {
-
+public class LengthComparison
+{
     public static void main(String[] args)
     {
         System.out.println("Welcome to Line Comparison Computation ");
-        
-        int x1,x2,y1,y2;
-        double length1;
+
+        int x1,x2,y1,y2,x3,x4,y3,y4;
+        double length1,length2;
         x1 = 20;
         x2 = 30;
         y1 = 10;
         y2 = 20;
+        x3 = 20;
+        x4 = 30;
+        y3 = 10;
+        y4 = 20;
         length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
         System.out.println("The length of line1 " + length1);
+        length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+        System.out.println("The length of line2 " + length2);
+
+        Double d1 = new Double(length1);
+        Double d2 = new Double(length2);
+        System.out.println(d1.equals(d2));
     }
 }
