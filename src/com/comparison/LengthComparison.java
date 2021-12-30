a018500b4bfae1c7a0ab257861f3c484ab135e70
Avinash <Avichauhan111196@gmail.com>
2021-12-30 22:31:25 +0530

    Added : Welcome to Line Comparison

---
+++ src/com/comparison/LengthComparison.java
@@ -0,0 +1,9 @@
+package com.comparison;
+
+public class LengthComparison {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Line Comparison Computation ");
+    }
+}
